3cbc4b2a7608de0ef02bda2e9cba0dd679183374
Hari <harijilla723@gmail.com>
2024-11-16 11:47:23 +0530

    pelican.py contents changed as my username

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,8 +1,8 @@
 
 
-AUTHOR          = '<your name>'  # use your name
-SITENAME        = 'title' # use title
-GITHUB_USERNAME = 'tactlabs' # use your username
+AUTHOR          = 'Hari'  # use your name
+SITENAME        = 'My Python Note Books' # use title
+GITHUB_USERNAME = 'hariz723' # use your username
 
 
 # Blogroll
@@ -12,7 +12,7 @@ LINKS = (
 
 # Social widget
 SOCIAL = (
-    ("GitHub", "https://github.com/<yourusername>/"),
+    ("GitHub", "https://github.com/hariz723/"),
     ("LinkedIn", "<linkedin handle>"),
 )
 
